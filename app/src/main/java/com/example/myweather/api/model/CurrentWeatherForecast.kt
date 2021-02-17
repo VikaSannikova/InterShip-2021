5e24c6510eff285473edd10f162615ac72261d94
Sannikova Viktoria <vsannikova@VSANNIKOVA-A1.local>
2021-02-17 20:29:12 +0300

    initial commit

---
+++ app/src/main/java/com/example/myweather/api/model/CurrentWeatherForecast.kt
@@ -0,0 +1,11 @@
+package smart.sprinkler.app.api.model
+
+import com.google.gson.annotations.SerializedName
+
+data class CurrentWeatherForecast(
+    val id: Long,
+    @SerializedName("main")
+    val weather: CurrentWeather,
+    @SerializedName("weather")
+    val weatherImage: List<WeatherImage>
+)
